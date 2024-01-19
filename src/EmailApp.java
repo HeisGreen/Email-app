ffee4e653ead6344a612ea1feeafb8682836990c
HeisGreen <104990241+HeisGreen@users.noreply.github.com>
2024-01-19 13:27:33 +0100

    emailApp

--- src/EmailApp.java
+++ src/EmailApp.java
@@ -3,5 +3,9 @@ public class EmailApp {
         Email em1 = new Email("Chido", "Green");
         em1.showInfo();
         System.out.println(em1.showInfo());
+
+        Email em2 = new Email("Grace", "Essien");
+        em2.showInfo();
+        System.out.println(em2.showInfo());
     }
 }
